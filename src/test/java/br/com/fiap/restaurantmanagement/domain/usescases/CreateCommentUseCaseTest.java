2773a3aec27180a26ebfb6767b7f0fc26d4bda6e
Eric Rangel <b.ericrangel@hotmail.com>
2024-03-25 19:27:01 -0300

    feat | RestaurantComment - Testes Unitários

---
+++ src/test/java/br/com/fiap/restaurantmanagement/domain/usescases/CreateCommentUseCaseTest.java
@@ -0,0 +1,107 @@
+package br.com.fiap.restaurantmanagement.domain.usescases;
+
+import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.CommentSaveAdapter;
+import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.interfaces.CommentRepository;
+import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.interfaces.RestaurantRepository;
+import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.interfaces.UserRepository;
+import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.models.RestaurantCommentModel;
+import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.models.RestaurantModel;
+import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.models.UserModel;
+import br.com.fiap.restaurantmanagement.domain.entities.Comment;
+import br.com.fiap.restaurantmanagement.domain.ports.outbound.SaveAdapterPort;
+import br.com.fiap.restaurantmanagement.domain.usecases.CreateCommentUseCase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class CreateCommentUseCaseTest {
+
+    @Mock
+    private CreateCommentUseCase createCommentUseCase;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RestaurantRepository restaurantRepository;
+
+    @Mock
+    private UserModel userModel;
+
+    @Mock
+    private RestaurantModel restaurantModel;
+
+    private AutoCloseable openMocks;
+
+    @BeforeEach
+    void setup() {
+        openMocks = MockitoAnnotations.openMocks(this);
+        SaveAdapterPort saveAdapterPort = new CommentSaveAdapter(
+                commentRepository, userRepository, restaurantRepository);
+        createCommentUseCase = new CreateCommentUseCase(saveAdapterPort);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        openMocks.close();
+    }
+
+    @Test
+    void shouldCreateComment() {
+        // arrange
+        var comment = createComment();
+        var commentModel = createCommentModel();
+
+        when(userRepository.getReferenceById(anyLong())).thenReturn(userModel);
+        when(restaurantRepository.getReferenceById(anyLong())).thenReturn(restaurantModel);
+        when(commentRepository.save(any(RestaurantCommentModel.class))).thenReturn(commentModel);
+
+        // Act
+        var commentSaved = createCommentUseCase.execute(comment);
+
+        // assert
+        assertThat(commentSaved).isNotNull();
+        assertThat(commentSaved.getComment()).isEqualTo(comment.getComment());
+
+        verify(userRepository, times(1)).getReferenceById(1L);
+        verify(restaurantRepository, times(1)).getReferenceById(1L);
+        verify(commentRepository, times(1)).save(any(RestaurantCommentModel.class));
+    }
+
+    private RestaurantCommentModel createCommentModel() {
+
+        var commentModel = new RestaurantCommentModel();
+
+        commentModel.setId(1L);
+        commentModel.setComment("Comment Exemple");
+        commentModel.setCreatedAt(LocalDateTime.now().withNano(0));
+        commentModel.setRestaurant(restaurantModel);
+        commentModel.setUser(userModel);
+
+        return commentModel;
+    }
+
+    private Comment createComment() {
+        return new Comment(
+                1L,
+                "Comment Exemple",
+                1L,
+                LocalDateTime.now());
+    }
+
+
+
+}
