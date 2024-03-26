9304dab9078f51f4a721757bf437e505c77d142a
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-03-26 19:53:15 -0300

    Refatorando código de criaçção reservas, atualizando teste unitário e adicionando tratamento de erros na criação de reservas

---
+++ src/test/java/br/com/fiap/restaurantmanagement/utils/ReservationHelper.java
@@ -0,0 +1,24 @@
+package br.com.fiap.restaurantmanagement.utils;
+
+import br.com.fiap.restaurantmanagement.adapter.inbound.controllers.dtos.request.CreateReservationRequest;
+import br.com.fiap.restaurantmanagement.domain.entities.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReservationHelper {
+
+  public static Reservation gerarReserva(String date, String time) {
+    return   CreateReservationRequest
+            .builder()
+            .name("Zeca")
+            .email("zeca@gmail.com")
+            .phoneNumber("11958974563")
+            .restaurantId(1L)
+            .places(1)
+            .date(LocalDate.parse(date))
+            .time(LocalTime.parse(time))
+            .build().toDomain();
+  }
+
+}
