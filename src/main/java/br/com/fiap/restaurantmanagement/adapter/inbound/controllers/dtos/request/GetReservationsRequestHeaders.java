a48c7d14d585a511fc8654beb0ce1952bb6ba91a
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-03-26 21:16:58 -0300

    Refatorando código de gerenciamento de reservas

--- src/main/java/br/com/fiap/restaurantmanagement/adapter/inbound/controllers/dtos/request/GetReservationsRequestHeaders.java
+++ src/main/java/br/com/fiap/restaurantmanagement/adapter/inbound/controllers/dtos/request/GetReservationsRequestHeaders.java
@@ -6,53 +6,34 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 @Data
 @Builder
 @NoArgsConstructor
 public class GetReservationsRequestHeaders {
 
   private Long restaurantId;
-  private LocalDate date;
-  private LocalTime time;
 
-  public GetReservationsRequestHeaders(
-          Long restaurantId,
-          LocalDate date,
-          LocalTime time) {
+  public GetReservationsRequestHeaders(Long restaurantId) {
 
-    validateMandatoryValues(restaurantId, date, time);
+    validateMandatoryValues(restaurantId);
 
     this.restaurantId = restaurantId;
-    this.date = date;
-    this.time = time;
 
   }
 
   public Reservation toDomain() {
 
     return new Reservation(
-            new ReservationRestaurant(restaurantId),
-            date,
-            time
+            new ReservationRestaurant(restaurantId)
     );
 
   }
 
-  private void validateMandatoryValues(
-          Long restaurantId,
-          LocalDate date,
-          LocalTime time) {
+  private void validateMandatoryValues(Long restaurantId) {
 
-    if (
-            restaurantId == null ||
-            date == null ||
-            time == null
-    ) {
+    if (restaurantId == null) {
 
-      throw new IllegalArgumentException("os campos não podem ser nulos");
+      throw new IllegalArgumentException("Fields cannot be null");
 
     }
 
--- src/main/java/br/com/fiap/restaurantmanagement/domain/usecases/DeleteReservationUseCase.java
+++ src/main/java/br/com/fiap/restaurantmanagement/domain/usecases/DeleteReservationUseCase.java
@@ -1,14 +1,9 @@
 package br.com.fiap.restaurantmanagement.domain.usecases;
 
 import br.com.fiap.restaurantmanagement.adapter.outbound.repositories.interfaces.ReservationRepository;
-import br.com.fiap.restaurantmanagement.domain.entities.Client;
 import br.com.fiap.restaurantmanagement.domain.entities.Reservation;
-import br.com.fiap.restaurantmanagement.domain.entities.ReservationRestaurant;
-import br.com.fiap.restaurantmanagement.domain.entities.Table;
+import br.com.fiap.restaurantmanagement.domain.exceptions.TransactionException;
 import br.com.fiap.restaurantmanagement.domain.ports.inbound.DeleteReservationUseCasePort;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class DeleteReservationUseCase implements DeleteReservationUseCasePort {
 
@@ -23,26 +18,19 @@ public class DeleteReservationUseCase implements DeleteReservationUseCasePort {
   @Override
   public Reservation delete(Long id) {
 
-    var deletedReservation = reservationRepository.findById(id).orElseThrow();
+    try {
 
-    var reservationToReturn =  new Reservation(
-            new ReservationRestaurant(
-                    deletedReservation.getRestaurant().getId(),
-                    new Table(
-                            deletedReservation.getTable().getId(),
-                            deletedReservation.getTable().getDescription(),
-                            deletedReservation.getNumberOfpeople())),
-            new Client(
-                    deletedReservation.getUser().getId(),
-                    deletedReservation.getUser().getName(),
-                    deletedReservation.getUser().getEmail(),
-                    deletedReservation.getUser().getPhoneNumber()),
-            LocalDate.parse(deletedReservation.getDate()),
-            LocalTime.parse(deletedReservation.getTime()));
+      var deletedReservation = reservationRepository.findById(id).orElseThrow();
 
-    reservationRepository.deleteById(id);
+      reservationRepository.deleteById(id);
 
-    return reservationToReturn;
+      return Reservation.toReservation(deletedReservation);
+
+    } catch (Exception exception) {
+
+      throw  new TransactionException("An error occurred while processing the reservation removal");
+
+    }
 
   }
 
